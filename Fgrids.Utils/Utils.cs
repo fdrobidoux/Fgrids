f53c9c5646d7f71c693e783923b1d66bde3b2465
Félix Dion-Robidoux <felix@fdrobidoux.com>
2019-08-27 14:22:09 -0400

    Stuff happened.

---
+++ Fgrids.Utils/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Fgrids.Utils
+{
+    public class Utils
+    {
+        public dynamic Employee = new ExpandoObject();
+
+        public Utils()
+        {
+            Employee.what = "what";
+        }
+    }
+}
